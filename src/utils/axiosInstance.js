c6ad703a4a4f566e6401fb3f579fbccb38a77eb7
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-07-09 16:05:08 +0900

    axiosInstance 수정

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // 백엔드 API의 기본 URL로 변경
+  baseURL: import.meta.env.VITE_API_URL, // 백엔드 API의 기본 URL로 변경
   headers: {
     'Content-Type': 'application/json',
     // 'Authorization': `Bearer ${localStorage.getItem('token')}` // 인증이 필요한 경우
