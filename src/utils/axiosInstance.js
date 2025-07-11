9b976b66593189e63b4b5a58f680855e720a9ed3
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-07-11 16:46:55 +0900

    actions 수정

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -20,34 +20,3 @@ const axiosInstance = axios.create({
 
 export default axiosInstance;
 
-
-
-
-// import axios from 'axios';
-
-// const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
-// // 필요에 따라 요청마다 헤더를 동적으로 추가할 수 있음
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   // 바디가 있는 POST 요청만 Content-Type 설정
-//   if (
-//     config.method === 'post' &&
-//     config.data &&
-//     typeof config.data === 'object' &&
-//     !(config.data instanceof FormData)
-//   ) {
-//     config.headers['Content-Type'] = 'application/json';
-//   }
-
-//   return config;
-// });
-
-// export default axiosInstance;
